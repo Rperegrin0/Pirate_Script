a6b0fd02db69624a06cc2d2ec1c28ffd7d86c271
Rperegrin0 <96554821+Rperegrin0@users.noreply.github.com>
2022-05-17 18:52:10 +0100

    Update piratescript.js

--- piratescript.js
+++ piratescript.js
@@ -1,5 +1,23 @@
 
 function piratize(input){
+    let final = "";
+    let translatedWords = []
+    let words = input.split('')
+    for (const word of words){
+        if(word == 'punds'){
+            translatedWords.push('doubloons')
+        }
+        else if (isNaN(word) = false){
+            translatedWords.push()
+        }
+        else{
+            let letters = word.split('')
+            let finalWord = letters.slice(1).join('') + letters[0] + 'arr'
+            translatedWords.push(finalWord)
+        }
+    }
+    return translatedWords.join('')
+    
 
     // add you code in here
     
